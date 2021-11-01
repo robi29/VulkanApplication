1cc81586ad49391fb192acc661ef6040966c2b6d
robi29 <robinson1994@interia.pl>
2021-11-01 23:10:32 +0100

    Added vertex duplication optimization.

--- VulkanApplication/VulkanApplication/main.h
+++ VulkanApplication/VulkanApplication/main.h
@@ -18,4 +18,12 @@ struct Vertex
     /// GetAttributeDescriptions.
     ////////////////////////////////////////////////////////////
     static auto GetAttributeDescriptions();
+
+    ////////////////////////////////////////////////////////////
+    /// Overrides == operator.
+    ////////////////////////////////////////////////////////////
+    bool operator==( const Vertex& other ) const
+    {
+        return position == other.position && color == other.color && textureCoordinate == other.textureCoordinate;
+    }
 };
--- VulkanApplication/VulkanApplication/tiny_obj_loader_library.cpp
+++ VulkanApplication/VulkanApplication/tiny_obj_loader_library.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#pragma warning( push )
+#pragma warning( disable : 4201 )
+#include <glm/gtx/hash.hpp>
+#pragma warning( pop )
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "Libraries/tiny_obj_loader.h"
 
 #include "main.h"
 #include "tiny_obj_loader_library.h"
+
+template <>
+struct std::hash<Vertex>
+{
+    std::size_t operator()( Vertex const& vertex ) const
+    {
+        const std::size_t hash1 = std::hash<glm::vec3>()( vertex.position );
+        const std::size_t hash2 = std::hash<glm::vec3>()( vertex.color ) << 1;
+        const std::size_t hash3 = std::hash<glm::vec2>()( vertex.textureCoordinate ) << 1;
+
+        return ( ( hash1 ^ hash2 ) >> 1 ) ^ hash3;
+    }
+};
 
 bool TinyObjLoader::loadModel( const std::string& fileName, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices )
 {
@@ -21,6 +40,8 @@ bool TinyObjLoader::loadModel( const std::string& fileName, std::vector<Vertex>&
         std::cerr << warn << " " << err << std::endl;
         return false;
     }
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices = {};
 
     for( const auto& shape : shapes )
     {
@@ -39,8 +60,13 @@ bool TinyObjLoader::loadModel( const std::string& fileName, std::vector<Vertex>&
                              1.0f,
                              1.0f };
 
-            vertices.push_back( vertex );
-            indices.push_back( static_cast<uint32_t>( indices.size() ) );
+            if( uniqueVertices.count( vertex ) == 0 )
+            {
+                uniqueVertices[vertex] = static_cast<uint32_t>( vertices.size() );
+                vertices.push_back( vertex );
+            }
+
+            indices.push_back( uniqueVertices[vertex] );
         }
     }
 
