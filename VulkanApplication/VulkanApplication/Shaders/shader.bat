1b100900b2623e81cc83207e4425343c69c11742
robi29 <robinson1994@interia.pl>
2023-09-07 18:55:20 +0200

    Updated Vulkan SDK to 1.3.261.1.

--- VulkanApplication/VulkanApplication/Shaders/shader.bat
+++ VulkanApplication/VulkanApplication/Shaders/shader.bat
@@ -1,4 +1,4 @@
-..\..\..\VulkanSDK\1.2.189.2\Bin\glslc.exe shader.vert -o vert.spv
-..\..\..\VulkanSDK\1.2.189.2\Bin\glslc.exe shader.frag -o frag.spv
-..\..\..\VulkanSDK\1.2.189.2\Bin\glslc.exe shader.comp -o comp.spv
+..\..\..\VulkanSDK\1.3.261.1\Bin\glslc.exe shader.vert -o vert.spv
+..\..\..\VulkanSDK\1.3.261.1\Bin\glslc.exe shader.frag -o frag.spv
+..\..\..\VulkanSDK\1.3.261.1\Bin\glslc.exe shader.comp -o comp.spv
 pause
